fbb3a3faa47705f6f72a014240483b9a18fddcf4
shubhammishra1023 <shubhammishra2056@gmail.com>
2026-01-27 23:23:30 +0545

    upgraded-api

--- check_models.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-API_KEY = "AIzaSyA57conYqxB1ZoKk4p4xaIv5OPZONNWDuw"  # <--- Paste your key here again
-genai.configure(api_key=API_KEY)
-
-print("Listing available models...")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
